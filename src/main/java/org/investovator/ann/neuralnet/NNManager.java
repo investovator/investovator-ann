f4a884618a2a6059ede7eabe1b51beebfe7433c2
hasalasurasinghe <hasala48@gmail.com>
2013-10-16 22:47:16 +0530

    Modified createNeuralNetwork method of NNManager

--- src/main/java/org/investovator/ann/neuralnet/NNManager.java
+++ src/main/java/org/investovator/ann/neuralnet/NNManager.java
@@ -20,7 +20,6 @@ package org.investovator.ann.neuralnet;
 
 import org.encog.neural.networks.BasicNetwork;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -32,22 +31,25 @@ import java.util.HashMap;
 public class NNManager {
 
     private HashMap<String,String> newParameters;
-    private ArrayList<String> inputParameters;
+    private String [] inputParameters;
     private String stockID;
     private NNCreator nnCreator;
     private NNTrainer nnTrainer;
     private boolean status;
 
-    public NNManager(){
-        nnCreator = new NNCreator(inputParameters.size(),1);
-        nnTrainer = new NNTrainer();
-        status = false;
-    }
-
-    public boolean createNeuralNetwork(HashMap newParameters,ArrayList inputParameters,String stockID){
+    public NNManager(HashMap newParameters,String [] inputParameters,String stockID){
         this.newParameters = newParameters;
         this.inputParameters = inputParameters;
         this.stockID = stockID;
+
+        status = false;
+    }
+
+    public boolean createNeuralNetwork(){
+
+        int inputParamCount = inputParameters.length + newParameters.size();
+        nnCreator = new NNCreator(inputParamCount,1);
+        nnTrainer = new NNTrainer();
 
         BasicNetwork network = nnCreator.createNetwork();
         status = nnTrainer.TrainANN(network,stockID);
